600fd6ff4445a64115f031a2c5758164de4ac9c0
Naoya Kohda <naoya.kohda@gmail.com>
2024-08-27 23:28:17 +0900

    mix new art_net

---
+++ lib/art_net.ex
@@ -0,0 +1,18 @@
+defmodule ArtNet do
+  @moduledoc """
+  Documentation for `ArtNet`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ArtNet.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
