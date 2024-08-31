3ff86989efc5c45d629e3d9e60e3c9ddd499f5e6
Naoya Kohda <naoya.kohda@gmail.com>
2024-08-31 20:27:44 +0900

    parse 処理を ArtNet.Packet に移す

---
+++ lib/art_net/packet.ex
@@ -0,0 +1,28 @@
+defmodule ArtNet.Packet do
+  @identifier "Art-Net" <> <<0>>
+  @version 0x14
+
+  def identifier, do: @identifier
+  def version, do: @version
+
+  @spec parse(module, binary) :: {:ok, struct} | :error
+  def parse(module, packet) do
+    module.schema()
+    |> Enum.reduce_while({[], packet}, fn {key, key_opts}, {values, rest} ->
+      {type, opts} = key_opts
+      size = Keyword.get(opts, :size)
+
+      case ArtNet.Decoder.parse(rest, type, size) do
+        {:ok, {value, new_rest}} ->
+          {:cont, {[{key, value} | values], new_rest}}
+
+        :error ->
+          {:halt, :error}
+      end
+    end)
+    |> case do
+      {data, ""} -> {:ok, struct!(module, data)}
+      _ -> :error
+    end
+  end
+end
--- lib/art_net/packet/schema.ex
+++ lib/art_net/packet/schema.ex
@@ -5,16 +5,8 @@ defmodule ArtNet.Packet.Schema do
   The schema is used to define the structure of Art-Net packets.
   """
 
-  @art_net_identifier "Art-Net" <> <<0>>
-  @packet_version 0x14
-
-  defp fetch_default_value({_type, opts}) do
-    Keyword.fetch(opts, :default)
-  end
-
-  defp fetch_default_value(_type) do
-    :error
-  end
+  @identifier ArtNet.Packet.identifier()
+  @version ArtNet.Packet.version()
 
   defmacro __using__(_opts \\ []) do
     quote do
@@ -24,19 +16,16 @@ defmodule ArtNet.Packet.Schema do
 
   defmacro defpacket(packet_schema) do
     header_schema = [
-      id: {:binary, default: @art_net_identifier, size: byte_size(@art_net_identifier)},
+      id: {:binary, default: @identifier, size: byte_size(@identifier)},
       op_code: {:integer, size: 16, little_endian: true},
-      version: {:integer, default: @packet_version, size: 16}
+      version: {:integer, default: @version, size: 16}
     ]
 
     schema = header_schema ++ packet_schema
 
     struct =
-      Enum.map(schema, fn {key, key_opts} ->
-        case fetch_default_value(key_opts) do
-          :error -> key
-          {:ok, default} -> {key, default}
-        end
+      Enum.map(schema, fn {key, {_type, field_opts}} ->
+        {key, Keyword.get(field_opts, :default)}
       end)
 
     types = Enum.map(schema, fn {key, {type, _}} -> {key, type} end)
@@ -51,35 +40,8 @@ defmodule ArtNet.Packet.Schema do
 
       @spec parse(binary) :: {:ok, t()} | :error
       def parse(packet) do
-        ArtNet.Packet.Schema.parse(__MODULE__, packet)
+        ArtNet.Packet.parse(__MODULE__, packet)
       end
-    end
-  end
-
-  @doc ~S"""
-  Returns the Art-Net identifier.
-  """
-  @spec art_net_identifier :: binary
-  def art_net_identifier, do: @art_net_identifier
-
-  @spec parse(module, binary) :: {:ok, struct} | :error
-  def parse(module, packet) do
-    module.schema()
-    |> Enum.reduce_while({[], packet}, fn {key, key_opts}, {values, rest} ->
-      {type, opts} = key_opts
-      size = Keyword.get(opts, :size)
-
-      case ArtNet.Decoder.parse(rest, type, size) do
-        {:ok, {value, new_rest}} ->
-          {:cont, {[{key, value} | values], new_rest}}
-
-        :error ->
-          {:halt, :error}
-      end
-    end)
-    |> case do
-      {data, ""} -> {:ok, struct!(module, data)}
-      _ -> :error
     end
   end
 end
