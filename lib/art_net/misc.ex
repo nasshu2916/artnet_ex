4b0e26be7e030214df0714b9f88d6989d7b98d60
Naoya Kohda <naoya.kohda@gmail.com>
2024-09-05 18:38:32 +0900

    末尾最適化されるように修正

--- lib/art_net/misc.ex
+++ lib/art_net/misc.ex
@@ -13,14 +13,20 @@ defmodule ArtNet.Misc do
       {:|, [], [:key1, {:|, [], [:key2, {:|, [], [:key3, :key4]}]}]}
   """
   @spec type_ast([atom]) :: any
-  def type_ast([key]), do: key
-  def type_ast(keys), do: parse_type_ast(keys)
-
-  defp parse_type_ast([left, right]) do
-    {:|, [], [left, right]}
+  def type_ast([key]) do
+    key
   end
 
-  defp parse_type_ast([left | rest]) do
-    {:|, [], [left, parse_type_ast(rest)]}
+  def type_ast(keys) do
+    [key | rest] = Enum.reverse(keys)
+    parse_type_ast(rest, key)
+  end
+
+  defp parse_type_ast([key], acc) do
+    {:|, [], [key, acc]}
+  end
+
+  defp parse_type_ast([key | rest], acc) do
+    parse_type_ast(rest, {:|, [], [key, acc]})
   end
 end
