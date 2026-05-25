057d6534e65e6976b78ff18f9b250d8425ba7c84
Naoya Kohda <naoya.kohda@gmail.com>
2026-05-26 04:51:45 +0900

    feat: ex_docのバージョンを0.40.2から0.40.3に更新し、古いバージョンの警告を追加

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule ArtNet.MixProject do
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:benchee, "~> 1.3", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.40.3", only: :dev, runtime: false, warn_if_outdated: true}
     ]
   end
 
