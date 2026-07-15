56a5835ed1854bfd9d8ae9cf7a245e5a7f1ad26a
naoya.kohda <naoya.kohda@gmail.com>
2026-07-16 01:37:05 +0900

    feat: 列挙型およびビットフィールドフィールドのエンコードとデコード処理を追加

--- test/art_net/packet/schema/code_generator_test.exs
+++ test/art_net/packet/schema/code_generator_test.exs
@@ -116,6 +116,57 @@ defmodule ArtNet.Packet.Schema.CodeGeneratorTest do
     Generator.generate([message: {{:string, nil}, []}], pre_decode_defined?: false)
   end
 
+  defmodule EnumAndBitFieldPacket do
+    @behaviour ArtNet.Packet.Schema
+
+    require Generator
+
+    defstruct [:priority, :talk_to_me]
+
+    @type t :: %__MODULE__{
+            priority: ArtNet.Packet.EnumTable.Priority.type(),
+            talk_to_me: ArtNet.Packet.BitField.TalkToMe.t()
+          }
+
+    @impl ArtNet.Packet.Schema
+    def validate(_), do: :ok
+
+    @impl ArtNet.Packet.Schema
+    def validate_decode(packet), do: validate(packet)
+
+    @impl ArtNet.Packet.Schema
+    def validate_encode(packet), do: validate(packet)
+
+    Generator.generate(
+      [
+        priority: {{:enum_table, ArtNet.Packet.EnumTable.Priority}, []},
+        talk_to_me: {{:bit_field, ArtNet.Packet.BitField.TalkToMe}, []}
+      ],
+      pre_decode_defined?: false
+    )
+  end
+
+  defmodule NestedListPacket do
+    @behaviour ArtNet.Packet.Schema
+
+    require Generator
+
+    defstruct [:items]
+
+    @type t :: %__MODULE__{items: [[integer]]}
+
+    @impl ArtNet.Packet.Schema
+    def validate(_), do: :ok
+
+    @impl ArtNet.Packet.Schema
+    def validate_decode(packet), do: validate(packet)
+
+    @impl ArtNet.Packet.Schema
+    def validate_encode(packet), do: validate(packet)
+
+    Generator.generate([items: {[[{:integer, 8}]], []}], pre_decode_defined?: false)
+  end
+
   test "generates default pre_decode when the packet does not define one" do
     assert GeneratedPacket.pre_decode(<<1, 2>>) == <<1, 2>>
   end
@@ -177,6 +228,70 @@ defmodule ArtNet.Packet.Schema.CodeGeneratorTest do
               }}
   end
 
+  test "generated encode body reports a non-list value" do
+    packet = %GeneratedPacket{
+      be: 1,
+      le: 0x0203,
+      data: <<0xAA, 0xBB>>,
+      name: "A",
+      items: :invalid
+    }
+
+    assert GeneratedPacket.__encode_body__(packet) ==
+             {:error, %ArtNet.EncodeError{reason: {:invalid_data, "items must be a list"}}}
+  end
+
+  test "generated encode body reports an invalid list length" do
+    packet = %GeneratedPacket{
+      be: 1,
+      le: 0x0203,
+      data: <<0xAA, 0xBB>>,
+      name: "A",
+      items: [4]
+    }
+
+    assert GeneratedPacket.__encode_body__(packet) ==
+             {:error,
+              %ArtNet.EncodeError{
+                reason: {:invalid_data, "items must contain 2 values, got 1"}
+              }}
+  end
+
+  test "generated encode body reports an invalid list element" do
+    packet = %GeneratedPacket{
+      be: 1,
+      le: 0x0203,
+      data: <<0xAA, 0xBB>>,
+      name: "A",
+      items: [4, 0x100]
+    }
+
+    assert GeneratedPacket.__encode_body__(packet) ==
+             {:error,
+              %ArtNet.EncodeError{
+                reason:
+                  {:encode_error,
+                   %{key: :items, type: [{:integer, 8}], value: [4, 0x100], element: 0x100}}
+              }}
+  end
+
+  test "generated encode body reports a nested list element that failed to encode" do
+    packet = %NestedListPacket{items: [[1, 0x100]]}
+
+    assert NestedListPacket.__encode_body__(packet) ==
+             {:error,
+              %ArtNet.EncodeError{
+                reason:
+                  {:encode_error,
+                   %{
+                     key: :items,
+                     type: [[{:integer, 8}]],
+                     value: [[1, 0x100]],
+                     element: [1, 0x100]
+                   }}
+              }}
+  end
+
   test "uses a packet-defined pre_decode when one exists" do
     assert PreDecodedPacket.pre_decode(<<5>>) == <<0, 5>>
     assert PreDecodedPacket.__decode_body__(<<5>>) == {:ok, %PreDecodedPacket{value: 5}}
@@ -202,5 +317,25 @@ defmodule ArtNet.Packet.Schema.CodeGeneratorTest do
     packet = %VariableStringPacket{message: "Art-Net"}
 
     assert VariableStringPacket.__encode_body__(packet) == {:ok, "Art-Net"}
+  end
+
+  test "generates decode and encode bodies for enum and bit-field fields" do
+    talk_to_me = %ArtNet.Packet.BitField.TalkToMe{
+      reply_on_change: true,
+      diagnostics: false,
+      diag_unicast: false,
+      vlc: true,
+      targeted_mode: false
+    }
+
+    packet = %EnumAndBitFieldPacket{priority: :dp_med, talk_to_me: talk_to_me}
+
+    assert EnumAndBitFieldPacket.__decode_body__(<<0x40, 0x12>>) == {:ok, packet}
+    assert EnumAndBitFieldPacket.__encode_body__(packet) == {:ok, <<0x40, 0x12>>}
+  end
+
+  test "generated decode body reports an invalid enum field" do
+    assert EnumAndBitFieldPacket.__decode_body__(<<0x01, 0x00>>) ==
+             {:error, %ArtNet.DecodeError{reason: {:decode_error, :priority}}}
   end
 end
